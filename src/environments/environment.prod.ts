720876b1f7912f18ecaeca7c9b6d36e6b99bb31d
angelsucasas <angel.alejandro.sucasas08@gmail.com>
2021-07-30 01:37:07 -0400

    adding variables to .env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,5 +4,6 @@ export const environment = {
   url: "https://security-camera-app-be.herokuapp.com:8000/api/",
   shortURL: "https://security-camera-app-be.herokuapp.com:8000",
   peerjsHost: "localhost",
-  peerJSPort: 3001
+  peerJSPort: 3001,
+  vapidPublicKey: "BPWkPcyZruyIUOSj6XWbltqNRDP5sfC2hO31tRQPGs9AgAkxPcxRqbMnAQiuPbdSZDqcgWggIBJ0IOWzvf0i4hw"
 };

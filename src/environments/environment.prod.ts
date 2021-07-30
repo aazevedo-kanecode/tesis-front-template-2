7c1daab465a555e31c96e76ca4e176b916909d98
angelsucasas <angel.alejandro.sucasas08@gmail.com>
2021-07-30 00:08:17 -0400

    fixing the .env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   contentType: "application/json",
   url: "https://security-camera-app-be.herokuapp.com:8000/api/",
-  shortURL: "https://security-camera-app-be.herokuapp.com/:8000",
+  shortURL: "https://security-camera-app-be.herokuapp.com:8000",
   peerjsHost: "localhost",
   peerJSPort: 3001
 };
